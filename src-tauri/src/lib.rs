686a68f2d8db1166c4c887f8d9599b7819049e38
lucia <bentancorlucia.u@gmail.com>
2026-01-19 19:52:47 -0300

    fix: remove updater plugin that prevents app from starting

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,7 +18,6 @@ pub fn run() {
                 .build(),
         )
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
         .setup(|app| {
